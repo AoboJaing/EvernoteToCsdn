c8bc9efe07a03dc40af19169ca8842203a850788
Aobo Jaing <744910955@qq.com>
2016-10-02 12:16:50 +0800

    find .md file all picture string
    Use Regular Expressions's re.findall() func get .md file all picture
    string. And print their.

--- evernoteToCsdn.py
+++ evernoteToCsdn.py
@@ -1,6 +1,11 @@
+import re
+
 f = open('D:\WorkSpace\python_ws\EvernoteToCsdn\EvernoteToCsdn\install_PCL.md', 'r')
 md = f.read()
 f.close()
 
-print md.decode('utf-8')
+# print md.decode('utf-8')
+
+a = re.findall('!\[Alt text\]\((.*?)\)', md, re.S)
+print a
 
